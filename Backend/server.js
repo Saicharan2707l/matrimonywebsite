2bdca8e46f182985e3ebfc988864de0065228a0c
PRASHANTH REDDY <81088623+prshanthreddy@users.noreply.github.com>
2024-01-02 16:39:21 -0600

    Modified the structure of the Codebase

--- Backend/server.js
+++ Backend/server.js
@@ -1,48 +1,35 @@
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const FormDataModel = require ('./models/FormData');
+const UserModel = require ('./models/User');
 const app = express();
 app.use(express.json());
 app.use(cors());
-mongoose.connect('mongodb://127.0.0.1:27017/logindetails');
-app.post('/register', (req, res)=>{
-    // To post / insert data into database
-    const {email, password} = req.body;
-    FormDataModel.findOne({email: email})
-    .then(user => {
-        if(user){
-            res.json("Already registered")
-        }
-        else{
-            FormDataModel.create(req.body)
-            .then(log_reg_form => res.json(log_reg_form))
-            .catch(err => res.json(err))
-        }
-    })
-    
-})
+mongoose.connect('mongodb://127.0.0.1:27017/loginDetails');
+app.post('/register', async (req, res) => {
+    UserModel.create(req.body)
+    .then(user=>res.json(user))
+    .catch(err=>res.status(400).json('Error: '+err));
+});
 
-app.post('/login', (req, res)=>{
-    // To find record from the database
+app.post('/login', async (req, res) => {
     const {email, password} = req.body;
-    FormDataModel.findOne({email: email})
-    .then(user => {
-        if(user){
-            // If user found then these 2 cases
-            if(user.password === password) {
+    UserModel.findOne({email: email})
+    .then(user=>{
+       if(user){
+              if(password===user.password){
                 res.json("Success");
-            }
-            else{
+              }else{
                 res.json("Wrong password");
+              }
+       }
+         else{
+              res.json("User does not exist");
             }
-        }
-        // If user not found then 
-        else{
-            res.json("No records found! ");
-        }
     })
-})
+}   
+);
+
 
 app.listen(3001, () => {
     console.log("Server listining on http://127.0.0.1:3001");
